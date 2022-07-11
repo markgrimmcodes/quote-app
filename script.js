48e5f5f25a6bc488aaa1444016112b9f33f8f191
Mark Grimm <mark.grimm.coding@gmail.com>
2022-07-11 14:41:43 +0200

    feat:adds functionality, needs styling

---
+++ script.js
@@ -0,0 +1,27 @@
+let quote = document.querySelector(".quote");
+let author = document.querySelector(".author");
+let btn = document.querySelector(".btn");
+
+fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
+  .then((response) => {
+    return response.json();
+  })
+  .then((dataObject) => {
+    console.log(dataObject.data[0].quoteText);
+    console.log(dataObject.data[0].quoteAuthor);
+    quote.innerText = dataObject.data[0].quoteText;
+    author.innerText = dataObject.data[0].quoteAuthor;
+  });
+
+btn.addEventListener("click", () => {
+  fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
+    .then((response) => {
+      return response.json();
+    })
+    .then((dataObject) => {
+      console.log(dataObject.data[0].quoteText);
+      console.log(dataObject.data[0].quoteAuthor);
+      quote.innerText = dataObject.data[0].quoteText;
+      author.innerText = dataObject.data[0].quoteAuthor;
+    });
+});
