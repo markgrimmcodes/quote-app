cf2934cb75c1cddde3bab0a6205211c69322b8b9
Mark Grimm <mark.grimm.coding@gmail.com>
2022-07-12 13:39:40 +0200

    feat: refactored

--- script.js
+++ script.js
@@ -2,18 +2,9 @@ let quote = document.querySelector(".quote");
 let author = document.querySelector(".author");
 let btn = document.querySelector(".btn");
 
-fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
-  .then((response) => {
-    return response.json();
-  })
-  .then((dataObject) => {
-    console.log(dataObject.data[0].quoteText);
-    console.log(dataObject.data[0].quoteAuthor);
-    quote.innerText = dataObject.data[0].quoteText;
-    author.innerText = "-" + dataObject.data[0].quoteAuthor;
-  });
+loadQuote();
 
-btn.addEventListener("click", () => {
+function loadQuote() {
   fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
     .then((response) => {
       return response.json();
@@ -24,4 +15,8 @@ btn.addEventListener("click", () => {
       quote.innerText = dataObject.data[0].quoteText;
       author.innerText = "-" + dataObject.data[0].quoteAuthor;
     });
+}
+
+btn.addEventListener("click", () => {
+  loadQuote();
 });
